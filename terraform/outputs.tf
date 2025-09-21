974800d2b120024b138ae75fa7c8bb19009f0a75
D-Stap <Davantestapleton49@gmail.com>
2025-09-21 15:05:27 -0400

    chore(CLOUD-37): update outputs.tf to test security scan detection

--- terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-aws_secret_access_key = "AKIAFAKEKEY1234567890"
-password              = "supersecret123"
-resource "aws_s3_bucket" "bad_bucket" {
-  bucket = "my-bad-bucket"
-  acl    = "public-read"
-}
